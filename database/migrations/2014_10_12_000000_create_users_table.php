3c0c8736ab49edadd1a2ba35fae4e90cacb0a76d
thanhvm97 <thanh1997>
2023-02-14 17:08:26 +0700

    plats user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,16 @@ return new class extends Migration
             $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->char('confirmation_code', 6)->after('email_verified_at')->nullable();
+            $table->string('twitter')->nullable()->comment('Link đến tài khoản twitter');
+            $table->string('facebook')->nullable()->comment('Link đến tài khoản facebook');
+            $table->string('discord')->nullable()->comment('Link đến tài khoản discord');
+            $table->string('telegram')->nullable()->comment('Link đến tài khoản telegram');
+            $table->integer('gender')->after('email_verified_at')->nullable()->comment('0: Nam, 1: Nu, 2: Khac');
+            $table->date('birth')->after('gender')->nullable()->comment('năm sinh');
+            $table->string('avatar_path')->after('birth')->nullable()->comment('ảnh đại diện');
+            $table->boolean('check_avatar')->after('avatar_path')->default(false)->comment('duyện ảnh đại diện');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
