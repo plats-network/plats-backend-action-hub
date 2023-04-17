89602138856a90c0cac0c9a775e3b24a1eeadbf8
thanhvm97 <thanh1997>
2023-04-17 16:05:02 +0700

    update preview

--- resources/views/admin/event/preview.blade.php
+++ resources/views/admin/event/preview.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div id="app">
-        <event-preview task_id="{{$task_id}}" link_cws="{{config('app.link_cws')}}"></event-preview>
+        <event-preview task_id="{{$task_id}}" link_cws="{{config('app.link_cws')}}" link_event="{{config('app.link_event')}}" link_mini_game="{{config('app.link_minigame')}}"></event-preview>
     </div>
 @endsection
