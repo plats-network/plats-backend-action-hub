845110bd2dc387b744f351c95ee2e70dccac2906
thanhvm97 <thanh1997>
2023-04-17 10:53:28 +0700

    update

--- resources/js/components/event/preview.vue
+++ resources/js/components/event/preview.vue
@@ -5,18 +5,18 @@
             <a href="/events" ><el-button type="primary" icon="el-icon-back"></el-button></a>
         </div>
         <el-row :gutter="20">
-            <el-col :span="12"  >
+            <el-col :span="10"  >
                 <div  class="p-2" style="border: 1px solid #EBEEF5">
-                    <div v-show="form.task_galleries.length > 0">
-                        <span>Slider</span>
-                        <el-carousel indicator-position="outside">
-                            <el-carousel-item  v-for="(event, index) in form.task_galleries" >
-                                <div>
-                                    <el-image :src="event.url" class=""></el-image>
-                                </div>
-                            </el-carousel-item>
-                        </el-carousel>
-                    </div>
+<!--                    <div v-show="form.task_galleries.length > 0">-->
+<!--                        <span>Slider</span>-->
+<!--                        <el-carousel indicator-position="outside">-->
+<!--                            <el-carousel-item  v-for="(event, index) in form.task_galleries" >-->
+<!--                                <div>-->
+<!--                                    <el-image :src="event.url" class=""></el-image>-->
+<!--                                </div>-->
+<!--                            </el-carousel-item>-->
+<!--                        </el-carousel>-->
+<!--                    </div>-->
                     <div class="demo-image">
                         <span>Image</span>
                         <div class="block">
@@ -63,103 +63,124 @@
                     </div>
                 </div>
             </el-col>
-            <el-col  :span="12" >
+            <el-col  :span="14" >
                 <div class="p-2" style="border: 1px solid #EBEEF5">
-                    <div v-if="form.sessions !== null">
-                        <span>Sessions</span>
-                        <el-card shadow="hover"  class="box-card mb-2" >
-                            <div slot="header" class="clearfix">
-                                <el-image :src="form.sessions.banner_url"></el-image>
-                                <br>
-                                <span><strong>Title :</strong>   {{form.sessions.name}}</span>
-                                <br>
-                                <span><strong>Max Job :</strong> {{form.sessions.max_job}}</span>
-                                <br>
-                                <span><strong>Description:</strong> <div v-html="form.sessions.description"></div></span>
-                            </div>
-                            <div v-for="detail in form.sessions.detail" class="text item mb-1">
-                                <el-alert
-                                    :closable="false"
-                                    :title="detail.name"
-                                    type="success"
-                                    show-icon>
-                                </el-alert>
-                            </div>
-                        </el-card>
-                    </div>
-                    <div  v-if="form.booths !== null">
-                        <span>Booths</span>
-                        <el-card shadow="hover" class="box-card mb-2" >
-                            <div slot="header" class="clearfix">
-                                <el-image :src="form.booths.banner_url"></el-image>
-                                <br>
-                                <span><strong>Title :</strong> {{form.booths.name}}</span>
-                                <br>
-                                <span><strong>Max Job :</strong> {{form.booths.max_job}}</span>
-                                <br>
-                                <span><strong>Description:</strong> <div v-html="form.booths.description"></div></span>
-                            </div>
-                            <div v-for="detail in form.booths.detail" class="text item mb-1">
-                                <el-alert
-                                    :closable="false"
-                                    :title="detail.name"
-                                    type="success"
-                                    show-icon>
-                                </el-alert>
-                            </div>
-                        </el-card>
-                    </div>
-                    <div v-if="form.quiz.length > 0">
-                        <span>Quiz</span>
-                        <el-card shadow="hover" v-for="detail in form.quiz"  class="box-card mb-2" >
-                            <div slot="header" class="clearfix">
-                                <span> Name : {{ detail.name}}</span>
-                                <span>Order : {{ detail.order}}</span>
-                                <span>Time : {{ detail.time_quiz}}</span>
-                                <span>Status : {{ detail.status}}</span>
-                            </div>
-                            <div v-for="detail in detail.detail" class="text item mb-1">
-                                <el-alert
-                                    :closable="false"
-                                    v-if="detail.status == true"
-                                    :title="detail.name"
-                                    type="success"
-                                    show-icon>
-                                </el-alert>
-                                <el-alert
-                                    :closable="false"
-                                    v-if="detail.status == false"
-                                    :title="detail.name"
-                                    type="info"
-                                    show-icon>
-                                </el-alert>
-                            </div>
-                        </el-card>
-                    </div>
-                    <div v-if="form.task_event_socials !== null">
-                        <span>Social</span>
-                        <el-card shadow="hover"  class="box-card mb-2" >
-                            <div slot="header" class="clearfix">
-                                <el-row :gutter="20" class="p-5">
-                                    <el-col >
-                                        <el-checkbox  v-model="form.task_event_socials.is_comment">Comment</el-checkbox>
-                                        <el-checkbox v-model="form.task_event_socials.is_like">Like</el-checkbox>
-                                        <el-checkbox v-model="form.task_event_socials.is_retweet">Retweet</el-checkbox>
-                                        <el-checkbox v-model="form.task_event_socials.is_tweet">Tweet</el-checkbox>
-                                    </el-col>
-                                    <el-col >
-                                        <div >
-                                            <span> URL</span>
-                                            <el-input class="mb-2 mt-2" maxlength="255" show-word-limit  placeholder="https://twitter.com/elonmusk/status/1638381090368012289" v-model="form.task_event_socials.url"></el-input>
+                    <div class="row">
+                        <div v-if="form.sessions !== null" class="col-md-6">
+                            <span>Sessions</span>
+                            <el-card shadow="hover" class="box-card mb-2">
+                                <div slot="header" class="clearfix">
+                                    <span><strong>Title :</strong>   {{ form.sessions.name }}</span>
+                                    <br>
+                                    <span><strong>Max Job :</strong> {{ form.sessions.max_job }}</span>
+                                </div>
+                                <div v-for="(detail, index) in form.sessions.detail" class="mb-1">
+                                    <el-popover
+                                        placement="right"
+                                        trigger="click">
+                                        <div ref="qrcode">
+                                            <qrcode-vue :id="detail.id" :value="link_event+'/events/code?type=event&id='+detail.code" :size="size" level="H"></qrcode-vue>
+                                            <button @click="downloadQrCode(detail.id)">Download</button>
                                         </div>
-                                        <div >
-                                            <span> Text</span>
-                                            <el-input class="mb-2 mt-2" maxlength="255" show-word-limit  placeholder="Text ..." v-model="form.task_event_socials.text"></el-input>
+                                        <el-alert slot="reference" style="cursor: pointer;" class="hover-alert"
+                                            :closable="false"
+                                            :title="index+1 + ' : ' + detail.name"
+                                            type="info"
+                                        >
+                                        </el-alert>
+                                    </el-popover>
+                                </div>
+                            </el-card>
+                        </div>
+                        <div v-if="form.booths !== null" class="col-md-6">
+                            <span>Booths</span>
+                            <el-card shadow="hover" class="box-card mb-2">
+                                <div slot="header" class="clearfix">
+                                    <span><strong>Title :</strong> {{ form.booths.name }}</span>
+                                    <br>
+                                    <span><strong>Max Job :</strong> {{ form.booths.max_job }}</span>
+                                </div>
+                                <div v-for="(detail, index) in form.booths.detail" class="text item mb-1">
+                                    <el-popover
+                                        placement="right"
+                                        trigger="click">
+                                        <div ref="qrcode">
+                                            <qrcode-vue :id="detail.id" :value="link_event+'/events/code?type=event&id='+detail.code" :size="size" level="H"></qrcode-vue>
+                                            <button @click="downloadQrCode(detail.id)">Download</button>
                                         </div>
-                                    </el-col>
-                                </el-row>
-                            </div>
-                        </el-card>
+                                        <el-alert slot="reference" style="cursor: pointer;" class="hover-alert"
+                                                  :closable="false"
+                                                  :title="index+1 + ' : ' + detail.name"
+                                                  type="info"
+                                        >
+                                        </el-alert>
+                                    </el-popover>
+                                </div>
+                            </el-card>
+                        </div>
+                        <div v-if="form.quiz.length > 0" class="col-md-6">
+                            <span>Quiz</span>
+                            <el-card shadow="hover" v-for="detail in form.quiz" class="box-card mb-2">
+                                <div slot="header" class="clearfix">
+                                    <span> Name : {{ detail.name }}</span>
+                                    <br>
+                                    <span>Order : {{ detail.order }}</span>
+                                    <br>
+                                    <span>Time : {{ detail.time_quiz }}</span>
+                                    <br>
+                                    <span>Status : {{ detail.status }}</span>
+                                </div>
+                                <div v-for="detail in detail.detail" class="text item mb-1">
+                                    <el-alert
+                                        :closable="false"
+                                        v-if="detail.status == true"
+                                        :title="detail.name"
+                                        type="success"
+                                        show-icon>
+                                    </el-alert>
+                                    <el-alert
+                                        :closable="false"
+                                        v-if="detail.status == false"
+                                        :title="detail.name"
+                                        type="info"
+                                        show-icon>
+                                    </el-alert>
+                                </div>
+                            </el-card>
+                        </div>
+                        <div v-if="form.task_event_socials !== null" class="col-md-6">
+                            <span>Social</span>
+                            <el-card shadow="hover" class="box-card mb-2">
+                                <div slot="header" class="clearfix">
+                                    <el-row :gutter="20">
+                                        <el-col>
+                                            <el-checkbox v-model="form.task_event_socials.is_comment">Comment
+                                            </el-checkbox>
+                                            <el-checkbox v-model="form.task_event_socials.is_like">Like
+                                            </el-checkbox>
+                                            <el-checkbox v-model="form.task_event_socials.is_retweet">Retweet
+                                            </el-checkbox>
+                                            <el-checkbox v-model="form.task_event_socials.is_tweet">Tweet
+                                            </el-checkbox>
+                                        </el-col>
+                                        <el-col>
+                                            <div>
+                                                <span> URL</span>
+                                                <el-input class="mb-2 mt-2" maxlength="255" show-word-limit
+                                                          placeholder="https://twitter.com/elonmusk/status/1638381090368012289"
+                                                          v-model="form.task_event_socials.url"></el-input>
+                                            </div>
+                                            <div>
+                                                <span> Text</span>
+                                                <el-input class="mb-2 mt-2" maxlength="255" show-word-limit
+                                                          placeholder="Text ..."
+                                                          v-model="form.task_event_socials.text"></el-input>
+                                            </div>
+                                        </el-col>
+                                    </el-row>
+                                </div>
+                            </el-card>
+                        </div>
                     </div>
                 </div>
             </el-col>
@@ -171,6 +192,7 @@
 import { ref } from 'vue'
 import 'element-tiptap/lib/index.css';
 import {Notification} from 'element-ui';
+import QrcodeVue from 'qrcode.vue'
 import {ElementTiptap} from 'element-tiptap';
 import {
     Doc,
@@ -213,12 +235,14 @@ import {
 import moment from "moment";
 export default {
     name: "preview",
-    props: ['task_id','link_cws'],
+    props: ['task_id','link_cws','link_event'],
     components: {
         'el-tiptap': ElementTiptap,
+        QrcodeVue,
     },
     data() {
         return {
+            size: 300,
             form : {
                 id : '',
                 banner_url : '',
@@ -301,6 +325,23 @@ export default {
         }
     },
     methods: {
+        downloadQrCode(id){
+            let canvasImage = document.getElementById(id).querySelector('canvas').toDataURL('image/png');
+            // this can be used to download any image from webpage to local disk
+            let xhr = new XMLHttpRequest();
+            xhr.responseType = 'blob';
+            xhr.onload = function () {
+                let a = document.createElement('a');
+                a.href = window.URL.createObjectURL(xhr.response);
+                a.download = 'qrc'+id+'.png';
+                a.style.display = 'none';
+                document.body.appendChild(a);
+                a.click();
+                a.remove();
+            };
+            xhr.open('GET', canvasImage); // This is to download the canvas Image
+            xhr.send();
+        },
         handleEditEvent(){
             window.location.href = '/events/edit/'+ this.task_id;
         },
@@ -338,5 +379,8 @@ export default {
 </script>
 
 <style scoped>
-
+.hover-alert:hover {
+    background-color: #409EFF;
+    color: white;
+}
 </style>
