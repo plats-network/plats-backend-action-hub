7784de855414060358d520d2a9313b5eccc68a96
Phan Xuân Dũng <dungpx@vaixgroup.com>
2023-12-11 09:14:53 +0700

    [Admin Form] Add Payment Link

--- resources/views/web/events/show.blade.php
+++ resources/views/web/events/show.blade.php
@@ -65,7 +65,7 @@
                                         <h3 class="title" title="{{$sponsor->name}}">{{$sponsor->name}}</h3>
                                         <p class="descs"  title="{{$sponsor->description}}">{{$sponsor->description}}</p>
                                         <div class="note">
-                                            <p class="price">Total Price: AZERO{{number_format($sponsor->price)}}</p>
+                                            <p class="price">Total Price: {{number_format($sponsor->price)}} AZERO</p>
                                             <p>Backers: 10+</p>
                                             <p>Est delivery: {{dateFormat($sponsor->end_at)}}</p>
                                         </div>
@@ -83,8 +83,8 @@
                                                 </div>
                                             </div>
                                             <div class="row">
-                                                <div class="col-md-4" style="padding-top: 8px; padding-right: 20px;">AZERO</div>
-                                                <div class="col-md-8">
+                                                <div class="col-md-5" style="padding-top: 8px; padding-right: 0px;">AZERO</div>
+                                                <div class="col-md-7" style="padding-left: 2px">
                                                     <input id="amount" class="form-control" type="text" name="price">
                                                     <input id="sponsorId" type="hidden">
                                                 </div>
@@ -106,7 +106,7 @@
                                         <div class="package-item">
                                             @foreach($sponsor->sponsorDetails as $item)
                                                 <div class="item price-package" data-price="{{number_format($item->price)}}" data-id="{{$item->id}}">
-                                                    <p>{{$item->name}} <span class="price">AZERO{{$item->price}}</span></p>
+                                                    <p>{{$item->name}} <span class="price">{{$item->price}}</span> AZERO</p>
                                                     <p class="desc">{{$item->description}}</p>
                                                     <hr>
                                                 </div>
