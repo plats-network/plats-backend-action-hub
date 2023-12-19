8965d1d73befafa34d1044670886f71004b4a35b
Phan Xuân Dũng <dungpx@vaixgroup.com>
2023-12-19 11:23:31 +0700

    [Ticket Form] Update Ticket Validate Form

--- resources/views/home/payment-success.blade.php
+++ resources/views/home/payment-success.blade.php
@@ -45,12 +45,7 @@
                                 <h3 class="text-gray-600" data-config-id="ord-date-header">Date</h3>
                                 <p class="text-blue-300 font-bold font-heading" data-config-id="ord-date">{{$dateNow}}</p>
                             </div>
-                            <a class="inline-flex mt-6 md:mt-0 w-full lg:w-auto justify-center items-center py-4 px-6 border hover:border-gray-500 rounded-md font-bold font-heading" href="#">
-                                <svg width="16" height="20" viewbox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg" data-config-id="auto-svg-1-2">
-                                    <path d="M1 1V0.25C0.585786 0.25 0.25 0.585786 0.25 1L1 1ZM15 19V19.75C15.4142 19.75 15.75 19.4142 15.75 19H15ZM1 19H0.25C0.25 19.4142 0.585786 19.75 1 19.75L1 19ZM10 1L10.5303 0.46967C10.3897 0.329018 10.1989 0.25 10 0.25V1ZM15 6H15.75C15.75 5.80109 15.671 5.61032 15.5303 5.46967L15 6ZM15 18.25H1V19.75H15V18.25ZM1.75 19V1H0.25V19H1.75ZM1 1.75H10V0.25H1V1.75ZM14.25 6V19H15.75V6H14.25ZM9.46967 1.53033L14.4697 6.53033L15.5303 5.46967L10.5303 0.46967L9.46967 1.53033ZM8.25 1V5H9.75V1H8.25ZM11 7.75H15V6.25H11V7.75ZM8.25 5C8.25 6.51878 9.48122 7.75 11 7.75V6.25C10.3096 6.25 9.75 5.69036 9.75 5H8.25Z" fill="black"></path>
-                                </svg>
-                                <span class="ml-4" data-config-id="order-action">View Invoice</span>
-                            </a>
+
                         </div>
 
                         <a class="block text-center px-8 py-4 bg-orange-300 hover:bg-orange-400 text-white font-bold font-heading uppercase rounded-md transition duration-200" href="#" data-config-id="primary-action">Go back Event</a>
