c8a1b041a11ff10324d6096e2b86854ba768c4fb
vaix-dungpa <dungpa@vaixgroup.com>
2024-03-28 13:33:40 +0700

    make controller

--- routes/api/beta.php
+++ routes/api/beta.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\beta\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('test', [TestController::class, 'index']);
 
 //tasks
 Route::group(['prefix' => 'tasks'], function () {
