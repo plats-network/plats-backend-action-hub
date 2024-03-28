381e355b7eabc7c22677dc58c9139a9ca4b36a15
vaix-dungpa <dungpa@vaixgroup.com>
2024-03-28 13:14:23 +0700

    make taskController

--- routes/api/beta.php
+++ routes/api/beta.php
@@ -2,4 +2,10 @@
 
 use App\Http\Controllers\Api\beta\TestController;
 use Illuminate\Support\Facades\Route;
+
 Route::get('test', [TestController::class, 'index']);
+
+//tasks
+Route::group(['prefix' => 'tasks'], function () {
+
+});
