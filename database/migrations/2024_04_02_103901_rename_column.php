947471bd64c692da6cd4e38a91e5e068c93208e1
vaix-dungpa <dungpa@vaixgroup.com>
2024-04-02 11:09:06 +0700

    edit migration

--- database/migrations/2024_04_02_103901_rename_column.php
+++ database/migrations/2024_04_02_103901_rename_column.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,8 +12,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::statement('ALTER TABLE user_codes CHANGE COLUMN hash_code color_code VARCHAR(255)');
-        Schema::statement('ALTER TABLE user_codes CHANGE COLUMN task_id task_event_id CHAR(36)');
+        DB::statement('ALTER TABLE user_codes CHANGE COLUMN hash_code color_code VARCHAR(255)');
+        DB::statement('ALTER TABLE user_codes CHANGE COLUMN task_id task_event_id CHAR(36)');
     }
 
     /**
@@ -20,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::statement('ALTER TABLE user_codes CHANGE COLUMN color_code hash_code VARCHAR(255)');
-        Schema::statement('ALTER TABLE user_codes CHANGE COLUMN task_event_id task_id CHAR(36)');
+        DB::statement('ALTER TABLE user_codes CHANGE COLUMN color_code hash_code VARCHAR(255)');
+        DB::statement('ALTER TABLE user_codes CHANGE COLUMN task_event_id task_id CHAR(36)');
     }
 };
